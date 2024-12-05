840805f0a70c64a29c8726fbf80a7fbca9e38d40
SamRobinson123 <156377261+SamRobinson123@users.noreply.github.com>
2024-12-04 21:39:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -921,6 +921,9 @@ def toggle_modal(n1, n2, is_open):
         return not is_open
     return is_open
 
-# Run the app
+import os
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get('PORT', 8050))  # Default to 8050 if PORT is not set
+    app.run_server(debug=False, host='0.0.0.0', port=port)
+
